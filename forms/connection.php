825f7b000e73820d9188e1e536a27c959f2b01d4
Ch. Sai Kumar <96567025+95027@users.noreply.github.com>
2023-10-01 20:55:56 +0530

    Add files via upload

---
+++ forms/connection.php
@@ -0,0 +1,11 @@
+<?php
+
+    $_server = "localhost";
+    $_user = "root";
+    $_pass = "";
+    $_name = "forms";
+
+    $_conn = "";
+
+    $_conn = mysqli_connect($_server, $_user, $_pass, $_name);
+?>
